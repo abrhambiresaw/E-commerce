f4fb1985f766251c696719b27ae1ecf3f4e50682
Abrham Biresaw <abrhambiresaw2112@gmail.com>
2026-06-28 11:52:17 +0300

    add Telebirr payment logo to asset exports

--- src/assets/frontend_assets/assets.js
+++ src/assets/frontend_assets/assets.js
@@ -70,7 +70,8 @@ import support_img from './support_img.png'
 import menu_icon from './menu_icon.png'
 import about_img from './about_img.png'
 import contact_img from './contact_img.png'
-import razorpay_logo from './razorpay_logo.png'
+import Flutterwave_logo from './Flutterwave_logo.png'
+import telebirr from './telebirr.png'
 import stripe_logo from './stripe_logo.png'
 import cross_icon from './cross_icon.png'
 
@@ -90,7 +91,8 @@ export const assets = {
     menu_icon,
     about_img,
     contact_img,
-    razorpay_logo,
+    Flutterwave_logo,
+    telebirr,
     stripe_logo,
     cross_icon
 }
