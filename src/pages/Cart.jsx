4f2ed40bb9e0d7a0e4ae245d7facb887826be8f3
Abrham Biresaw <abrhambiresaw2112@gmail.com>
2026-07-09 10:07:28 +0300

    update the cart

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -26,7 +26,7 @@ function Cart() {
   },[cartItems]);
 
   return (
-    <div border-t pt-14>
+    <div className='border-t pt-14'>
       <div className="text-2xl mb-3">
         <Title text1={'YOUR'} text2={'CART'} />
       </div>
@@ -36,7 +36,7 @@ function Cart() {
             const productData = products.find((product) => product._id === item._id);
 
           return (
-            <div key={index} className='py-4 border-t border-b text-gray-700 grid-cols-[4fr_0.5fr_0.5fr} sm:grid-cols-[4fr_2fr_0.5fr] items-center gap-4'>
+            <div key={index} className='py-4 border-t border-b text-gray-700 grid grid-cols-[4fr_0.5fr_0.5fr} sm:grid-cols-[4fr_2fr_0.5fr] items-center gap-4'>
               <div className='flex items-start gap-6'>
                 <img className='w-16 sm:w-20' src={productData.image[0]} alt="" />
                 <div>
@@ -47,7 +47,7 @@ function Cart() {
                   </div>
                 </div>
               </div>
-              <input className='border max-w-10 sm:max-w-20 px-1 sm:px-2 py-1' type="number" min={1} defaultValue={item.quantity} />
+              <input onChange={(e)=>e.target.value ==='' || e.target.value === '0' ? null : updateQuantity(item._id,item.size,Number(e.target.value))} className='border max-w-10 sm:max-w-20 px-1 sm:px-2 py-1' type="number" min={1} defaultValue={item.quantity} />
               <img onClick={()=>updateQuantity(item._id, item.size,0)} className='w-4 mr-4 sm:w-5 cursor-pointer' src={assets.bin_icon} alt="" />
             </div>
           )
