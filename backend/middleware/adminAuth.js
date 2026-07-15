f6dd39b7d670b37b7d8f19470cc00a8e36644059
Abrham Biresaw <abrhambiresaw2112@gmail.com>
2026-07-15 13:46:13 +0300

    Enhance JWT token structure for admin authorization

--- backend/middleware/adminAuth.js
+++ backend/middleware/adminAuth.js
@@ -1,20 +1,25 @@
-import jwt from 'jsonwebtoken'
+import jwt from "jsonwebtoken";
 
-const adminAuth = async (req,res,next) => {
+const adminAuth = async (req, res, next) => {
   try {
     const { token } = req.headers;
     if (!token) {
-      return res.json({success:false,message:"Not Authorize Login Again"})
+      return res.json({ success: false, message: "Not Authorize Login Again" });
     }
-    const token_decode = jwt.verify(token,process.env.JWT_SECRET);
-    if(token_decode !== process.env.ADMIN_EMAIL + process.env.ADMIN_PASSWORD) {
-      return res.json({success:false,message:"Not Authorize, Login Again"})
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    if (decoded.role !== "admin") {
+      return res.json({
+        success: false,
+        message: "Not Authorized",
+      });
     }
-    next()
+
+    next();
   } catch (error) {
     console.log(error);
-    res.json({success:false,message:error.message})
+    res.json({ success: false, message: error.message });
   }
-}
+};
 
 export default adminAuth;
